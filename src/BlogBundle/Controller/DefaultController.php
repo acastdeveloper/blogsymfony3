91397b647b3cd4e2f9bbc6c8322fe3df0c25b867
acastdeveloper <ac.desenvolupador@gmail.com>
2017-06-19 21:53:41 +0200

    DefaultController modificat

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -3,77 +3,79 @@
 namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexOld()
     {
-
+		/*
+		 * 
+		 
+		$em = $this->getDoctrine()->getEntityManager();
+		$entry_repo = $em->getRepository("BlogBundle:Entry");
+		$entries = $entry_repo->findAll();
+		
+		foreach ($entries as $entry){
+			echo $entry->getTitle()."<br/>";
+			echo $entry->getCategory()->getName()."<br/>";
+			echo $entry->getUser()->getName()."<br/>";
+			
+			$tags = $entry->getEntryTag();
+			foreach ($tags as $tag){
+				echo $tag->getTag()->getName().", ";
+			}
+			
+			echo "<hr/>";
+		}
+		 * 
+		 */
+		
         /*
-        //PRIMERA PROVA, D'EXTREURE ATRIBUTS D'ENTRADA I FINS I TOT ATRIBUTS DE CATEGORIA
-        $em = $this->getDoctrine()->getEntityManager();
-        //Instancia un objecte del gestor d'entitats.
-        $entry_repo = $em->getRepository("BlogBundle:Entry");
-        //Instancia un objecte del repositori de la classe entry.
-
-        $entries =$entry_repo->findAll();
-        //Sembla l'equivalent a un SELECT *
-        //Obté una matriu d'objectes.
-        echo "<br>";
-
-        foreach($entries as $entry) {
-            echo "Títol: ".$entry->getTitle()."<br/>";
-            echo "Categoria:".$entry->getCategory()->getName()."<br>";
-            echo "Autor:".$entry->getUser()->getName();
-
-            $tags =$entry->getEntryTag();
-            foreach ($tags as $tag) {
-                echo $tag->getTag()->getName().", ";
-            }
-            echo "<hr>";
-
-        }
-        */
-
-
-        /*
-        $em = $this->getDoctrine()->getEntityManager();
-        $category_repo = $em->getRepository("BlogBundle:Category");
-        $categories =$category_repo->findAll();
-
-        foreach($categories as $category){
-            echo $category->getName()."<br>";
-            foreach($entries as $entry) {
-                echo getEntries()->getTitle();
-            }
-        }
-        */
-
-        $em = $this->getDoctrine()->getEntityManager();
-        
-        $category_repo = $em->getRepository("BlogBundle:Category");
-        $categories = $category_repo->findAll();
-        foreach ($categories as $category){
-            echo $category->getName()."<br/>";
-
-            $entries = $category->getEntries();
-            foreach ($entries as $entry){
-                echo $entry->getTitle().", ";
-            }
-
-            echo "<hr/>";
-        }
-
-
-
-
-
-
-
-
-
-        die();
+		
+		$em = $this->getDoctrine()->getEntityManager();
+		$category_repo = $em->getRepository("BlogBundle:Category");
+		$categories = $category_repo->findAll();
+		
+		foreach ($categories as $category){
+			echo $category->getName()."<br/>";
+			
+			$entries = $category->getEntries();
+			foreach ($entries as $entry){
+				echo $entry->getTitle().", ";
+			}
+			
+			echo "<hr/>";
+		}
+		*/
+		
+		$em = $this->getDoctrine()->getEntityManager();
+		$tag_repo = $em->getRepository("BlogBundle:Tag");
+		$tags = $tag_repo->findAll();
+		
+		foreach ($tags as $tag){
+			echo $tag->getName()."<br/>";
+			
+			$entryTag = $tag->getEntryTag();
+			foreach ($entryTag as $entry){
+				echo $entry->getEntry()->getTitle().", ";
+			}
+			
+			echo "<hr/>";
+		}
+		
+		die();
         return $this->render('BlogBundle:Default:index.html.twig');
     }
-
+	
+	public function indexAction(){
+		
+		return $this->render('BlogBundle:Default:index.html.twig');
+	}
+	
+	public function langAction(Request $request){
+		
+		return $this->redirectToRoute("blog_homepage");
+	}
 }
